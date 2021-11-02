6ff558d39f00b23d8fe6f7758f74ab9a66ea7f2b
Adela Selimović <adelaadria1@outlook.com>
2021-11-02 01:51:11 +0100

    Update App.tsx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -15,32 +15,14 @@ function App() {
     })
   }, []);
 
-  console.log("activities", activities)
   return (
     <>
       <NavBar/>
       <Container style={{marginTop: '7em'}}>
         { <ActivityDashboard activities={activities} />}
-        {/* <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List> */}
       </Container>
     </>
   );
 }
-{/* <NavBar/>
-       <Container style={{marginTop: '7em'}}>
-        <List>
-          {activities.map((activity: any) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
-        </Container> */}
 
 export default App;
